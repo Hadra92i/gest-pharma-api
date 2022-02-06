b3ea24a612d032681d715c25424c7585d88eff34
Hadra 92i <hadraiizi547@gmail.com>
2022-02-06 23:57:04 +0000

    first commit

---
+++ db/config.js
@@ -0,0 +1,8 @@
+import mongoose from 'mongoose';
+
+mongoose.connect('mongodb://localhost:27017/pharma_db',
+    { useNewUrlParser: true, useUnifiedTopology: true },
+    (err) => {
+        if (!err) console.log("Connection To Mongo DB Successfully Established");
+        else console.log("Connection error : ", err);
+    });
