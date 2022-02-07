3630b94c35619d885455927e99362c3aef44d157
Hadra 92i <hadraiizi547@gmail.com>
2022-02-07 19:34:48 +0000

    feat(logger): added express morgan

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const MedocController = require('./controllers/MedocController');
 const PharmacieController = require('./controllers/PharmacieController');
+const morgan = require('morgan');
 require('./db/config');
 
 const app = express();
@@ -10,8 +11,9 @@ const PORT = 5000;
 
 app.use(cors());
 app.use(bodyParser.json());
+app.use(morgan('tiny'));
 
-app.use('/api', MedocController);
-app.use('/api', PharmacieController);
+app.use('/api/medocs', MedocController);
+app.use('/api/pharmacies', PharmacieController);
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
