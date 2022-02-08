26587b2665589676a32573088ffb75bf586e3788
Hadra 92i <hadraiizi547@gmail.com>
2022-02-08 20:23:44 +0000

    fixed the bug add pharmacy

--- controllers/PharmacieController.js
+++ controllers/PharmacieController.js
@@ -13,16 +13,12 @@ router.get('/', (req, res) => {
 router.post('/', (req, res) => {
     const newPharmacie = new Pharmacie({
         name_fr: req.body.name_fr,
-        name_ar: req.body.name_ar,
         adress_fr: req.body.adress_fr,
-        adress_ar: req.body.adress_ar,
         tel: req.body.tel,
         lati: req.body.lati,
         longt: req.body.longt,
         wilaya: req.body.wilaya,
-        wilaya_ar: req.body.wilaya_ar,
         moughataa: req.body.moughataa,
-        moughataa_ar: req.body.moughataa_ar
     });
 
     newPharmacie.save((err, newPharmacie) => {
